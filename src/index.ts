05c9a5abde56f61dfadf95a94ff023d9e3c8e0d0
mdskfaisal <mdskfaisal@live.com>
2024-07-09 11:48:43 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,60 +13,77 @@ import { CLEANUP_NAME } from "./constants";
 
 export async function run(): Promise<void> {
     try {
-        // Default client region is set by configure-aws-credentials
+        core.debug('Starting run function');
+        
+        core.debug('Creating SecretsManagerClient');
         const client : SecretsManagerClient = new SecretsManagerClient({region: process.env.AWS_DEFAULT_REGION, customUserAgent: "github-action"});
+        
+        core.debug('Getting secret-ids input');
         const secretConfigInputs: string[] = [...new Set(core.getMultilineInput('secret-ids'))];
+        
+        core.debug('Getting parse-json-secrets input');
         const parseJsonSecrets = core.getBooleanInput('parse-json-secrets');
+        
+        core.debug('Getting name-transformation input');
         const nameTransformation = parseTransformationFunction(core.getInput('name-transformation'));
 
-        // Get final list of secrets to request
+        core.debug('Building secrets list');
         core.info('Building secrets list...');
         const secretIds: string[] = await buildSecretsList(client, secretConfigInputs, nameTransformation);
 
-        // Keep track of secret names that will need to be cleaned from the environment
+        core.debug('Initializing secretsToCleanup array');
         let secretsToCleanup = [] as string[];
 
+        core.debug('Logging info about secret name transformation');
         core.info('Your secret names may be transformed in order to be valid environment variables (see README). Enable Debug logging in order to view the new environment names.');
 
-        // Get and inject secret values
+        core.debug('Starting loop to process each secret');
         for (let secretId of secretIds) {
-            //  Optionally let user set an alias, i.e. `ENV_NAME,secret_name`
+            core.debug(`Processing secret: ${secretId}`);
+            
+            core.debug('Extracting alias and secretId');
             let secretAlias: string | undefined = undefined;
             [secretAlias, secretId] = extractAliasAndSecretIdFromInput(secretId, nameTransformation);
 
-            // Retrieves the secret name also, if the value is an ARN
+            core.debug(`Checking if secretId is ARN: ${secretId}`);
             const isArn = isSecretArn(secretId);
 
             try {
+                core.debug(`Getting secret value for: ${secretId}`);
                 const secretValueResponse : SecretValueResponse = await getSecretValue(client, secretId);
                 const secretValue = secretValueResponse.secretValue;
 
-                // Catch if blank prefix is specified but no json is parsed to avoid blank environment variable
+                core.debug('Checking for blank prefix and JSON parsing');
                 if ((secretAlias === '') && !(parseJsonSecrets && isJSONString(secretValue))) {
+                    core.debug('Setting secretAlias to undefined due to blank prefix');
                     secretAlias = undefined;
                 }
 
+                core.debug('Setting secretAlias if undefined');
                 if (secretAlias === undefined) {
                     secretAlias = isArn ? secretValueResponse.name : secretId;
                 }
 
+                core.debug(`Injecting secret: ${secretAlias}`);
                 const injectedSecrets = injectSecret(secretAlias, secretValue, parseJsonSecrets, nameTransformation);
+                core.debug('Updating secretsToCleanup');
                 secretsToCleanup = [...secretsToCleanup, ...injectedSecrets];
             } catch (err) {
-                // Fail action for any error
+                core.debug(`Error fetching secret: ${secretId}`);
                 core.setFailed(`Failed to fetch secret: '${secretId}'. Error: ${err}.`)
             } 
         }
 
-        // Export the names of variables to clean up after completion
+        core.debug('Exporting cleanup variable');
         core.exportVariable(CLEANUP_NAME, JSON.stringify(secretsToCleanup));
 
+        core.debug('Logging completion message');
         core.info("Completed adding secrets.");
     } catch (error) {
+        core.debug('Caught error in run function');
         if (error instanceof Error) core.setFailed(error.message)
     }
 }
 
-
-
+core.debug('Calling run function');
 run();
